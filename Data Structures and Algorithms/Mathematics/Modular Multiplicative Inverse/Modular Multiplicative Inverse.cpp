e0db1cf80c3cb2858489a76928cdec22f00182f4
Glauco Acassio <69947061+glaucoacassio@users.noreply.github.com>
2020-09-16 19:27:02 -0300

    Create Modular Multiplicative Inverse.cpp

---
+++ Data Structures and Algorithms/Mathematics/Modular Multiplicative Inverse/Modular Multiplicative Inverse.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int modInverse(int a, int m)
+{
+    for(int i=1; i<=m-1; i++)
+        if( ( (a*i) % m ) == 1 ) 
+        return i;
+    
+    return -1;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+	int T;
+	
+	cin>>T;
+	while(T--){
+		int a,m;
+		
+		cin>>a>>m;
+		
+		
+		cout << modInverse(a, m)<<endl;
+	}
+    return 0;
+} 
